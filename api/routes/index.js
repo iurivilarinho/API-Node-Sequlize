fddecdf307cec3061a6b20d947ff9d9c4d607305
iuri Vilarinho <iuri-vilarinho@hotmail.com>
2022-09-21 17:12:59 -0300

    ControllersAll

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,19 +1,12 @@
-//routes/index.js
-
 const bodyParser = require('body-parser')
-
 const pessoas = require('./pessoasRoute')
-// rotas de niveis e turmas
 const niveis = require('./niveisRoute')
 const turmas = require('./turmasRoute')
 
-//instâncias de níveis e turmas
-//e refatoramos um pouco a função
 module.exports = app => {
- app.use(
-   bodyParser.json(),
-   pessoas,
-   niveis,
-   turmas
-   )
- }
+  app.use(bodyParser.json())
+  app.use(pessoas)
+  app.use(niveis)
+  app.use(turmas)
+  
+}
--- api/routes/turmasRoute.js
+++ api/routes/turmasRoute.js
@@ -4,10 +4,10 @@ const { Router } = require('express')
 const TurmaController = require('../controllers/TurmaController')
 
 const router = Router()
-router
- .get('/turmas', TurmaController.pegaTodasAsTurmas)
- .get('/turmas/:id', TurmaController.pegaUmaTurma)
- .post('/turmas', TurmaController.criaTurma)
- .put('/turmas/:id', TurmaController.atualizaTurma)
- .delete('/turmas/:id', TurmaController.apagaTurma)
+
+router.get('/turmas', TurmaController.pegaTodasAsTurmas)
+router.get('/turmas/:id', TurmaController.pegaUmaTurma)
+router.post('/turmas', TurmaController.criaTurma)
+router.put('/turmas/:id', TurmaController.atualizaTurma)
+router.delete('/turmas/:id', TurmaController.apagaTurma)
 module.exports = router
